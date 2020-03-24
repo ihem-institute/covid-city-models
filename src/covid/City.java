0cc4ebb3435837c80a3400d7f86f72d22361a84e
Gabriel Flores <eldalai@gmail.com>
2020-03-24 15:07:46 -0300

    basic infection simulation

--- src/covid/City.java
+++ src/covid/City.java
@@ -1,5 +1,8 @@
 package covid;
 
+import cern.jet.random.Exponential;
+import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.random.RandomHelper;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.grid.Grid;
 
@@ -8,12 +11,33 @@ public class City {
 	private ContinuousSpace<Object> space;
 	private Grid<Object> grid;
 	private int population, infected;
+	private Exponential infectionPrevision = RandomHelper.createExponential(2);
 
 	public City(ContinuousSpace<Object> space, Grid<Object> grid, int population, int infected) {
 		this.space = space;
 		this.grid = grid;
-		this.population = population;
+		this.setPopulation(population);
+		this.setInfected(infected);
+	}
+
+	@ScheduledMethod(start = 1, interval = 1)
+	public void step() {
+		this.setInfected(this.getInfected() + infectionPrevision.nextInt());
+	}
+
+	public int getInfected() {
+		return infected;
+	}
+
+	public void setInfected(int infected) {
 		this.infected = infected;
 	}
 
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
 }
