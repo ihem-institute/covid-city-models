a1ddf64b57c4791c735dec6646e9c02efb1c7a87
Gabriel Flores <eldalai@gmail.com>
2020-03-28 13:31:20 -0300

    apply integration to models

--- src/covid/City.java
+++ src/covid/City.java
@@ -5,6 +5,8 @@ import java.util.Iterator;
 import java.util.List;
 
 import cern.jet.random.Exponential;
+import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.engine.schedule.ISchedule;
 import repast.simphony.engine.schedule.ScheduledMethod;
 import repast.simphony.random.RandomHelper;
 import repast.simphony.space.continuous.ContinuousSpace;
@@ -14,10 +16,17 @@ public class City {
 
 	private ContinuousSpace<Object> space;
 	private Grid<Object> grid;
-	private int population, infected;
+	private long population;
+	private long infected;
 	private String name;
 	private Exponential infectionPrevision = RandomHelper.createExponential(2);
 	private List<CityLink> links = new ArrayList<CityLink>();
+	private long dead;
+	private long hospital;
+	private long recovered;
+	private long infectious;
+	private long exposed;
+	private double[] lastSeir = Covid.SEIR20.clone();
 
 	public City(ContinuousSpace<Object> space, Grid<Object> grid, String name, int population, int infected) {
 		this.name = name;
@@ -33,20 +42,41 @@ public class City {
 
 	@ScheduledMethod(start = 1, interval = 1)
 	public void step() {
-		this.setInfected(this.getInfected() + infectionPrevision.nextInt());
+//		this.setInfected(this.getInfected() + infectionPrevision.nextInt());
+		ISchedule schedule = RunEnvironment.getInstance().getCurrentSchedule();
+		int tickCount = (int) schedule.getTickCount() % 1000000000;
+
+		Covid.apply_integration(this.lastSeir, 0.5, 0);
 		
+        //    
+		this.dead = (long) (this.population * this.lastSeir[9]);  
+		this.hospital = (long) (this.population * this.lastSeir[5]+this.lastSeir[6]);         
+		this.recovered = (long) (this.population * this.lastSeir[7] + this.lastSeir[8]);       
+		this.infectious = (long) (this.population * this.lastSeir[2]);  
+		this.exposed = (long) (this.population * this.lastSeir[1]);
+		
+//		this.population -= transitPeople;
+		this.infected = (int) (this.population * (1-this.lastSeir[0]));
+
 		for (CityLink cityLink : links) {
-			int transitInfected = this.infected * (cityLink.getTransitAverage()/1000) * (cityLink.getOpenessStrategy() / 100);
-			int transitPeople = cityLink.getTransitAverage() * (cityLink.getOpenessStrategy() / 100);
-			
-			this.population -= transitPeople;
-			this.infected -= transitInfected;
+//			int transitInfected = this.infected * (cityLink.getTransitAverage()/1000) * (cityLink.getOpenessStrategy() / 100);
+//			int transitPeople = cityLink.getTransitAverage() * (cityLink.getOpenessStrategy() / 100);
 			
 			cityLink.getOtherCity(this).transit(
-				transitPeople,  
-				transitInfected
+				0, //transitPeople,  
+				0 //transitInfected
 			);
 		}
+		System.out.println(
+			"(" + tickCount + ") " +
+			this.name + ": " +
+			" dead: " + this.dead + 
+			" hospital: " + this.hospital +
+			" recovered: " + this.recovered +
+			" infectious: " + this.infectious +
+			" exposed " + this.exposed +
+			" infected: " + this.infected
+		);
 	}
 
 	private void transit(int transitPeople, int transitInfected) {
@@ -54,7 +84,7 @@ public class City {
 		this.population += transitPeople;
 	}
 
-	public int getInfected() {
+	public double getInfected() {
 		return infected;
 	}
 
@@ -62,7 +92,7 @@ public class City {
 		this.infected = infected;
 	}
 
-	public int getPopulation() {
+	public long getPopulation() {
 		return population;
 	}
 
@@ -84,5 +114,25 @@ public class City {
 
 	public void setLinks(List<CityLink> links) {
 		this.links = links;
+	}
+
+	public long getDead() {
+		return dead;
+	}
+
+	public long getHospital() {
+		return hospital;
+	}
+
+	public long getRecovered() {
+		return recovered;
+	}
+
+	public long getInfectious() {
+		return infectious;
+	}
+
+	public long getExposed() {
+		return exposed;
 	}
 }
--- src/covid/Covid.java
+++ src/covid/Covid.java
@@ -11,16 +11,24 @@ public class Covid {
 
 	long Time_to_death = 32;
 	double logN = 15.761420707019587;
-	double N = 6999999.999999998;
-	int I0 = 1;
-	double R0 = 2.2;
-	double D_incbation = 5.2;
-	double D_infectious = 2.9;
-	double D_recovery_mild = 11.1;
-	double D_recovery_severe = 28.6;
-	double D_hospital_lag = 5, D_death = 29.1, CFR = 0.02, InterventionTime = 100,
-			OMInterventionAmt = 0.6666666666666666, InterventionAmt = 0.33333333333333337, Time = 220, Xmax = 110000,
-			dt = 2, P_SEVERE = 0.2;
+	public static final  double N = 6999999.999999998;
+	public static final  int I0 = 1;
+	public static final double [] SEIR20 = {1-I0/N, 0, I0/N, 0, 0, 0, 0, 0, 0, 0};
+	static double R0 = 2.2;
+	static double D_incbation = 5.2;
+	static double D_infectious = 2.9;
+	static double D_recovery_mild = 11.1;
+	static double D_recovery_severe = 28.6;
+	static double D_hospital_lag = 5;
+	static double D_death = 29.1;
+	static double CFR = 0.02;
+	double InterventionTime = 100;
+	double OMInterventionAmt = 0.6666666666666666;
+	double InterventionAmt = 0.33333333333333337;
+	double Time = 220;
+	double Xmax = 110000;
+	double dt = 2;
+	static double P_SEVERE = 0.2;
 	long duration = 840000000000L;
 //		state =  "http = //gabgoh.github.io/COVID/index.html?CFR=0.02&D_hospital_lag=5&D_incbation=5.2&D_infectious=2.9&D_recovery_mild=11.1&D_recovery_severe=28.6&I0=1&InterventionAmt=0.33333333333333337&InterventionTime=100&P_SEVERE=0.2&R0=2.2&Time_to_death=32&logN=15.761420707019587"
 //		Sol =  undefined
@@ -47,42 +55,44 @@ public class Covid {
 //		milestones =  undefined
 	boolean log = true;
 	
-	public void integrate() {
-		double [] SEIR20 = {1-I0/N, 0, I0/N, 0, 0, 0, 0, 0, 0, 0};
+	public List<double[]> integrate() {
 		double NSTEPS = 10000;
 		double TTOT = 200;
 		
 		List<double []> output = new ArrayList<double[]>();
-		
-//		sol2 = np.zeros((NSTEPS,10))
-		
+				
 		double[] lastSeir = SEIR20.clone();
 		output.add(lastSeir.clone());
+		double d = TTOT/NSTEPS;
 		for(int t = 0; t <= NSTEPS; t++) {
-			double[] result = f(t, lastSeir);
-			double [] subTotal = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-			for(int index = 0; index < 10; index ++) {
-				subTotal[index] = (TTOT/NSTEPS) * result[index];
-				lastSeir[index] = lastSeir[index] + subTotal[index];
-				System.out.print(lastSeir[index]+", ");
-			}
-			System.out.println();
+			apply_integration(lastSeir, d, t);
 			output.add(lastSeir.clone());
 		}
+		return output;
+	}
+	public static void apply_integration(double[] lastSeir, double d, int t) {
+		double[] result = f(t, lastSeir);
+		double [] subTotal = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		for(int index = 0; index < 10; index ++) {
+			subTotal[index] = d * result[index];
+			lastSeir[index] = lastSeir[index] + subTotal[index];
+//			System.out.print(lastSeir[index]+", ");
+		}
+//		System.out.println();
 	}
 	
 	
-	public double [] f(int t, double[] x){
+	private static double [] f(int t, double[] x){
 
 	      // SEIR ODE
 	      double beta;
-		if (t > InterventionTime && t < InterventionTime + duration){
-	        beta = (InterventionAmt)*R0/(D_infectious);
-	      } else if (t > InterventionTime + duration) {
-	        beta = 0.5*R0/(D_infectious);        
-	      } else {
+//		if (t > InterventionTime && t < InterventionTime + duration){
+//	        beta = (InterventionAmt)*R0/(D_infectious);
+//	      } else if (t > InterventionTime + duration) {
+//	        beta = 0.5*R0/(D_infectious);        
+//	      } else {
 	        beta = R0/(D_infectious);
-	      }
+//	      }
 	      double a = 1/D_incbation;
 	      double gamma = 1/D_infectious;
 	      
